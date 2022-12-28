10a2101d1a6d7986b08e86850e6e55f87a05e1ce
신요한 <johnyohan93@gmail.com>
2022-12-28 17:34:13 +0900

    output Management

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import _ from "lodash";
+import printMe from "./print";
+
 export function component() {
   const element = document.createElement("div");
+  const button = document.createElement("button");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+  button.innerHTML = "Clcik Me and check the console!!!";
+  button.onclick = printMe;
+
+  element.append(button);
 
   return element;
 }
